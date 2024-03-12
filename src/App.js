3ca0556b74767f1b20a20b387c7433192fc263bb
Kulik Leonid <lyonyauei@gmail.com>
2024-03-12 18:56:47 +0100

    RHR-1: init

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
+import React from "react";
+
+import JokeList from "./components/JokeList";
+import "./App.css";
+
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    // const dummyJokes = [
+    //   {
+    //     id: 1,
+    //     type: "general",
+    //     setup: "What do you call a bee that lives in America?",
+    //     punchline: "A USB.",
+    //   },
+    //   {
+    //     id: 2,
+    //     type: "programming",
+    //     setup: "What's the best thing about a Boolean?",
+    //     punchline: "Even if you're wrong, you're only off by a bit.",
+    //   },
+    // ];
+
+    // let
+
+    function fetchJokesHandler() {
+        fetch('https://v2.jokeapi.dev/joke/Any?amount=10')
+            .then(response => {
+                return response.json();
+            }).then(data => {
+
+            })
+        ;
+    }
+
+    return (
+        <React.Fragment>
+            <section>
+                <button>Fetch Jokes</button>
+            </section>
+            <section>
+                <JokeList jokes={dummyJokes}/>
+            </section>
+        </React.Fragment>
+    );
 }
 
 export default App;
